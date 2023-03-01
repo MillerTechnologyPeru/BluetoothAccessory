ea13206d1ad33d8abfc3953916b40b354db6d498
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-02-28 23:59:06 -0800

    Add `InformationService`

---
+++ Sources/BluetoothAccessory/Services/InformationService.swift
@@ -0,0 +1,15 @@
+//
+//  InformationService.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 2/28/23.
+//
+
+import Foundation
+
+public struct InformationService {
+    
+    public static var type: ServiceType { .information }
+    
+    
+}
