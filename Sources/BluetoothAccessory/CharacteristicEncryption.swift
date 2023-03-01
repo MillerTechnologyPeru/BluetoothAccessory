7e2ac99685d94b6bd582d8468f7e0481697d9108
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-02-28 22:15:57 -0800

    Add `CharacteristicEncryption`

---
+++ Sources/BluetoothAccessory/CharacteristicEncryption.swift
@@ -0,0 +1,17 @@
+//
+//  CharacteristicEncryption.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 2/28/23.
+//
+
+import Foundation
+
+/// Characteristic Encryption
+public enum CharacteristicEncryption: UInt8, CaseIterable {
+    
+    case `none`
+    case authentication
+    case encryptedData
+    
+}
