56e8877a47a7b41a9a20a8ea04f104022136bf8a
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-02-28 22:13:55 -0800

    Renamed `BluetoothAccessoryError`

---
+++ Sources/BluetoothAccessory/Error.swift
@@ -0,0 +1,28 @@
+//
+//  Error.swift
+//
+//
+//  Created by Alsey Coleman Miller on 2/28/23.
+//
+
+import Foundation
+import Bluetooth
+
+/// Bluetooth Accessory Error
+public enum BluetoothAccessoryError: Error {
+    
+    /// No service with UUID found.
+    case serviceNotFound(BluetoothUUID)
+    
+    /// No characteristic with UUID found.
+    case characteristicNotFound(BluetoothUUID)
+    
+    /// The characteristic's value could not be parsed. Invalid data.
+    case invalidCharacteristicValue(BluetoothUUID)
+    
+    /// Not a compatible peripheral
+    case incompatiblePeripheral(Error?)
+    
+    /// Invalid data.
+    case invalidData(Data?)
+}
