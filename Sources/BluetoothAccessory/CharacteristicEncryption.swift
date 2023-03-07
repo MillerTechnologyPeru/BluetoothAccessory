91a2cdb1b17d30e5e382ba6a386ddbc1f6bf1070
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-06 23:25:42 -0800

    Updated `CharacteristicEncryption`

--- Sources/BluetoothAccessory/CharacteristicEncryption.swift
+++ Sources/BluetoothAccessory/CharacteristicEncryption.swift
@@ -10,8 +10,12 @@ import Foundation
 /// Characteristic Encryption
 public enum CharacteristicEncryption: UInt8, Codable, CaseIterable {
     
+    /// Normal GATT read or write, ideal for small values.
     case `none`
-    case authentication
+    
+    /// Authentication and encrypted data, ideal for small values. Read or write.
     case encryptedData
-    case encryptedChunks
+    
+    /// Authentication and encryped data partitioned in chunks. Read or write. Data type must be TLV8.
+    case encryptedList
 }
