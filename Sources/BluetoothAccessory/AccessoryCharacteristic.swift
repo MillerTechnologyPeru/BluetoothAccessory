cdacda17dac2d4bd520aff667142f377f3bddd57
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-14 11:30:41 -0700

    Add `GATTAttribute.Characteristic.init()`

--- Sources/BluetoothAccessory/AccessoryCharacteristic.swift
+++ Sources/BluetoothAccessory/AccessoryCharacteristic.swift
@@ -8,6 +8,9 @@
 import Foundation
 import Bluetooth
 import GATT
+#if canImport(BluetoothGATT)
+import BluetoothGATT
+#endif
 
 /// Bluetooth Accessory Characteristic
 public protocol AccessoryCharacteristic {
@@ -16,9 +19,7 @@ public protocol AccessoryCharacteristic {
     
     static var type: CharacteristicType { get }
         
-    static var properties: Bluetooth.BitMaskOptionSet<GATT.Characteristic.Property> { get }
-        
-    static var encryption: CharacteristicEncryption { get }
+    static var properties: BitMaskOptionSet<CharacteristicProperty> { get }
     
     static var unit: CharacteristicUnit? { get }
         
@@ -46,3 +47,43 @@ public extension AccessoryCharacteristic {
         value.characteristicValue.encode()
     }
 }
+
+public extension AccessoryCharacteristic {
+    
+    static var descriptors: [AccessoryDescriptor] {
+        [
+            .encryption(encryption),
+            .format(Value.characteristicFormat),
+            unit.flatMap({ .unit($0) })
+        ]
+        .compactMap { $0 }
+    }
+}
+
+#if canImport(BluetoothGATT)
+public extension AccessoryCharacteristic {
+    
+    static var permissions: Bluetooth.BitMaskOptionSet<GATTAttribute.Descriptor.Permission> {
+        var permissions = Bluetooth.BitMaskOptionSet<GATTAttribute.Descriptor.Permission>()
+        return permissions
+    }
+}
+
+public extension GATTAttribute.Characteristic {
+    
+    init<T: AccessoryCharacteristic>(_ characteristic: T.Type) {
+        var descriptors = characteristic.descriptors.map({ .init($0) })
+            + [GATTUserDescription(userDescription: characteristic.type.description).descriptor]
+        if characteristic.encryption != .none {
+            descriptors.append(GATTFormatDescriptor(format: .init(bluetoothAccessory: T.Value.format), exponent: 0, unit: 0, namespace: 0, description: 0).descriptor)
+        }
+        self.init(
+            uuid: BluetoothUUID(characteristic: characteristic.type),
+            value: Data(),
+            permissions: characteristic.permissions,
+            properties: characteristic.properties,
+            descriptors: descriptors
+        )
+    }
+}
+#endif
