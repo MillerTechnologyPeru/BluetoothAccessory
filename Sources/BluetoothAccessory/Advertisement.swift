fea974ffddb7221fdb3c5fd0f491909d4e3e909f
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-14 22:14:19 -0700

    Add `LowEnergyAdvertisingData.init(beacon:name:)`

--- Sources/BluetoothAccessory/Advertisement.swift
+++ Sources/BluetoothAccessory/Advertisement.swift
@@ -11,36 +11,62 @@ import Bluetooth
 import BluetoothHCI
 import BluetoothGAP
 
+public extension LowEnergyAdvertisingData {
+    
+    init(
+        beacon: AccessoryBeacon,
+        rssi: Int8,
+        flags: GAPFlags = [.lowEnergyGeneralDiscoverableMode, .notSupportedBREDR]
+    ) {
+        self.init(beacon: AppleBeacon(bluetoothAccessory: beacon, rssi: rssi), flags: flags)
+    }
+    
+    init(
+        service: ServiceType,
+        name: String
+    ) {
+        let maxLength = LowEnergyAdvertisingData.capacity - 16
+        var name = name
+        if name.utf8.count > maxLength {
+            name = String(name.prefix(maxLength))
+        }
+        let encoder = GAPDataEncoder()
+        let localName = GAPCompleteLocalName(name: name)
+        let serviceUUID: GAPIncompleteListOf128BitServiceClassUUIDs = [UUID(service: service)]
+        self = try! encoder.encodeAdvertisingData(localName, serviceUUID)
+    }
+}
+
 public extension BluetoothHostControllerInterface {
     
-    /// LE Advertise with iBeacon
-    func setAdvertisingData(beacon uuid: UUID, rssi: Int8) async throws {
+    /// LE Advertise with iBeacon for accessory.
+    func setAdvertisingData(
+        beacon accessoryBeacon: AccessoryBeacon,
+        rssi: Int8,
+        flags: GAPFlags = [.lowEnergyGeneralDiscoverableMode, .notSupportedBREDR],
+        interval: AdvertisingInterval = .min
+    ) async throws {
         
         do { try await enableLowEnergyAdvertising(false) }
         catch HCIError.commandDisallowed { }
         
-        let beacon = AppleBeacon(uuid: uuid, rssi: rssi)
-        let flags: GAPFlags = [.lowEnergyGeneralDiscoverableMode, .notSupportedBREDR]
-        
-        try await iBeacon(beacon, flags: flags, interval: .min)
+        let beacon = AppleBeacon(bluetoothAccessory: accessoryBeacon, rssi: rssi)
+        try await iBeacon(beacon, flags: flags, interval: interval)
         
         do { try await enableLowEnergyAdvertising() }
         catch HCIError.commandDisallowed { }
     }
     
     /// Set scan response with service UUID and name.
-    func setScanResponse(service uuid: UUID, name: String) async throws {
+    func setScanResponse(
+        service: ServiceType,
+        name: String
+    ) async throws {
         
         do { try await enableLowEnergyAdvertising(false) }
         catch HCIError.commandDisallowed { }
         
-        let name = GAPCompleteLocalName(name: name)
-        let serviceUUID: GAPIncompleteListOf128BitServiceClassUUIDs = [uuid]
-        
-        let encoder = GAPDataEncoder()
-        let data = try encoder.encodeAdvertisingData(name, serviceUUID)
-        
-        try await setLowEnergyScanResponse(data)
+        try await setLowEnergyScanResponse(.init(service: service, name: name))
         
         do { try await enableLowEnergyAdvertising() }
         catch HCIError.commandDisallowed { }
