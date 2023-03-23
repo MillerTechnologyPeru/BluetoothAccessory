ce2acc4115b1acbccb34e84f9ca18956116663a8
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-22 20:22:30 -0700

    Updated `InformationService`

--- Sources/BluetoothAccessory/Services/InformationService.swift
+++ Sources/BluetoothAccessory/Services/InformationService.swift
@@ -8,56 +8,67 @@
 import Foundation
 import Bluetooth
 
-public struct InformationService: AccessoryService, Identifiable {
+/// Accessory Information Service
+public actor InformationService <Peripheral: AccessoryPeripheralManager> : AccessoryService {
     
     public static var type: BluetoothUUID { BluetoothUUID(service: .information) }
     
-    @ManagedCharacteristic<IdentifierCharacteristic>
+    public let serviceHandle: UInt16
+    
+    @ManagedCharacteristic<IdentifierCharacteristic, Peripheral>
     public var id: UUID
     
-    @ManagedCharacteristic<NameCharacteristic>
+    @ManagedCharacteristic<NameCharacteristic, Peripheral>
     public var name: String
     
-    @ManagedCharacteristic<AccessoryTypeCharacteristic>
+    @ManagedCharacteristic<AccessoryTypeCharacteristic, Peripheral>
     public var accessoryType: AccessoryType
     
-    @ManagedCharacteristic<IdentifyCharacteristic>
-    public var identify: Bool = false
+    @ManagedCharacteristic<IdentifyCharacteristic, Peripheral>
+    public var identify: Bool
     
-    /*
-    public var manufacturer: String
+    public var manufacturer: String = ""
     
-    public var model: String
+    public var model: String = ""
     
-    public var serialNumber: String
+    public var serialNumber: String = ""
     
-    public var configuredName: String?
+    public var configuredName: String = ""
+        
+    public var hardwareVersion: String = ""
     
-    public var firmwareVersion: String?
+    public var softwareVersion: String = ""
     
-    public var hardwareVersion: String?
-    
-    public var softwareVersion: String?
-    */
+    public init(
+        peripheral: Peripheral,
+        id: UUID,
+        name: String,
+        accessoryType: AccessoryType
+    ) async throws {
+        let (serviceHandle, valueHandles) = try await peripheral.add(
+            service: InformationService.self,
+            with: [
+                IdentifierCharacteristic.self,
+                NameCharacteristic.self,
+                AccessoryTypeCharacteristic.self,
+                IdentifyCharacteristic.self
+            ]
+        )
+        self.serviceHandle = serviceHandle
+        _id = await .init(wrappedValue: id, peripheral: peripheral, valueHandle: valueHandles[0])
+        _name = await .init(wrappedValue: name, peripheral: peripheral, valueHandle: valueHandles[1])
+        _accessoryType = await .init(wrappedValue: accessoryType, peripheral: peripheral, valueHandle: valueHandles[2])
+        _identify = await .init(wrappedValue: false, peripheral: peripheral, valueHandle: valueHandles[3])
+    }
 }
 
 public extension InformationService {
     
-    static var characteristics: [any AccessoryCharacteristic.Type] {
-        [
-            IdentifierCharacteristic.self,
-            NameCharacteristic.self,
-            AccessoryTypeCharacteristic.self,
-            IdentifyCharacteristic.self,
-        ]
-    }
-    
     var characteristicValues: [ManagedCharacteristicValue] {
-        [
-            $id,
-            $name,
-            $accessoryType,
-            $identify
-        ]
+        get async {
+            [
+                $id
+            ]
+        }
     }
 }
