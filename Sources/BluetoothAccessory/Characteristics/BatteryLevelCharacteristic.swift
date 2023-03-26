efaae131f840349c6fb18c02b0ea67af93a939ab
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-26 12:15:54 -0700

    Add `BatteryLevelCharacteristic.unit`

--- Sources/BluetoothAccessory/Characteristics/BatteryLevelCharacteristic.swift
+++ Sources/BluetoothAccessory/Characteristics/BatteryLevelCharacteristic.swift
@@ -9,18 +9,26 @@ import Foundation
 import Bluetooth
 import GATT
 
+/// Accessory battery level value.
 public struct BatteryLevelCharacteristic: Equatable, Hashable, AccessoryCharacteristic {
-        
+    
     public static var type: BluetoothUUID { BluetoothUUID(characteristic: .batteryLevel) }
     
     public static var properties: BitMaskOptionSet<CharacteristicProperty> { [.read, .encrypted] } // TODO: Notifications
+    
+    public static var unit: CharacteristicUnit? { .percentage }
     
     public init(value: UInt8 = 100) {
         assert(value <= 100)
         self.value = value
     }
     
-    public var value: UInt8 = 100 // TODO: Validate battery level under 100%
+    public var value: UInt8 {
+        didSet {
+            // Validate battery level under 100%
+            assert(value <= 100, "Battery level must not exceed 100%")
+        }
+    }
 }
 
 // MARK: - Central
