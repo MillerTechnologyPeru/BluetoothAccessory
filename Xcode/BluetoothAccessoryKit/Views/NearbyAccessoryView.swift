1a73ba87ac6746bf9277cc41fd5bbfeac242493d
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-26 19:57:59 -0700

    Add `NearbyAccessoryView`

---
+++ Xcode/BluetoothAccessoryKit/Views/NearbyAccessoryView.swift
@@ -0,0 +1,99 @@
+//
+//  NearbyAccessoryView.swift
+//  BluetoothAccessoryKit
+//
+//  Created by Alsey Coleman Miller on 3/26/23.
+//
+
+import Foundation
+import SwiftUI
+import Bluetooth
+import GATT
+import DarwinGATT
+import BluetoothAccessory
+
+public struct NearbyAccessoryView: View {
+    
+    @EnvironmentObject
+    var store: AccessoryManager
+    
+    let peripheral: AccessoryManager.Peripheral
+    
+    let scanResponse: AccessoryScanResponse
+    
+    public init(
+        peripheral: AccessoryManager.Peripheral,
+        scanResponse: AccessoryScanResponse
+    ) {
+        self.peripheral = peripheral
+        self.scanResponse = scanResponse
+    }
+    
+    public var body: some View {
+        StateView(
+            peripheral: peripheral,
+            scanResponse: scanResponse,
+            manufacturerData: store[manufacturerData: peripheral],
+            beacon: store[beacon: peripheral],
+            key: accessoryID.flatMap { store.keys[$0] }
+        )
+    }
+}
+
+internal extension NearbyAccessoryView {
+    
+    var accessoryID: UUID? {
+        store[manufacturerData: peripheral]?.id ?? store[beacon: peripheral]?.uuid
+    }
+}
+
+internal extension NearbyAccessoryView {
+    
+    struct StateView: View {
+        
+        let peripheral: AccessoryManager.Peripheral
+        
+        let scanResponse: AccessoryScanResponse
+        
+        let manufacturerData: AccessoryManufacturerData?
+        
+        let beacon: AccessoryBeacon?
+        
+        let key: Key?
+        
+        var body: some View {
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    if let accessoryType = manufacturerData?.accessoryType {
+                        Text("Type: \(accessoryType.description)")
+                    }
+                    Text("Service: \("\(scanResponse.service)")")
+                    #if DEBUG
+                    Text("Peripheral: \(peripheral.description)")
+                    #endif
+                    if let id = accessoryID {
+                        Text("ID: \(id)")
+                    }
+                    if let isConfigured = manufacturerData?.isConfigured {
+                        Text("Configured: \(isConfigured.description)")
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
+            .padding(20)
+            .navigationTitle(Text(verbatim: title))
+        }
+    }
+}
+
+internal extension NearbyAccessoryView.StateView {
+    
+    var title: String {
+        scanResponse.name
+    }
+    
+    var accessoryID: UUID? {
+        manufacturerData?.id ?? beacon?.uuid
+    }
+}
