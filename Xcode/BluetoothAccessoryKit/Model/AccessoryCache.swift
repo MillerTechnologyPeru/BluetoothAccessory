daf5bee24224418779e28d6d7a134bf450c4106c
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-03-29 14:56:12 -0700

    Add `AccessoryCache`

---
+++ Xcode/BluetoothAccessoryKit/Model/AccessoryCache.swift
@@ -0,0 +1,36 @@
+//
+//  AccessoryCache.swift
+//  BluetoothAccessoryKit
+//
+//  Created by Alsey Coleman Miller on 3/29/23.
+//
+
+import Foundation
+import Bluetooth
+import BluetoothAccessory
+
+public struct AccessoryCache: Equatable, Hashable, Codable, JSONFile {
+    
+    /// Date application data was created.
+    public let created: Date
+    
+    /// Date application data was last modified.
+    public private(set) var updated: Date
+    
+    /// Persistent lock information.
+    public var accessories: [UUID: AccessoryInformation] {
+        didSet { if accessories != oldValue { didUpdate() } }
+    }
+    
+    /// Update date when modified.
+    private mutating func didUpdate() {
+        updated = Date()
+    }
+    
+    /// Initialize a new application data.
+    public init() {
+        self.created = Date()
+        self.updated = Date()
+        self.accessories = [:]
+    }
+}
