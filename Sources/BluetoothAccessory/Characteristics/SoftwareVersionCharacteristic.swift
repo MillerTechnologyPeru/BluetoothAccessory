34b7e545de1d63bef9dce12dbefc2b72a0251121
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-01 21:25:18 -0700

    Add `CentralManager.readSoftwareVersion()`

--- Sources/BluetoothAccessory/Characteristics/SoftwareVersionCharacteristic.swift
+++ Sources/BluetoothAccessory/Characteristics/SoftwareVersionCharacteristic.swift
@@ -21,3 +21,25 @@ public struct SoftwareVersionCharacteristic: Equatable, Hashable, AccessoryChara
     
     public var value: String
 }
+
+// MARK: - Central
+
+public extension CentralManager {
+    
+    /// Read manufacturer.
+    func readSoftwareVersion(
+        characteristic: Characteristic<Peripheral, AttributeID>
+    ) async throws -> String {
+        let characteristic = try await read(SoftwareVersionCharacteristic.self, characteristic: characteristic)
+        return characteristic.value
+    }
+}
+
+public extension GATTConnection {
+    
+    /// Read manufacturer.
+    func readSoftwareVersion() async throws -> String {
+        let characteristic = try self.cache.characteristic(.softwareVersion, service: .information)
+        return try await self.central.readSoftwareVersion(characteristic: characteristic)
+    }
+}
