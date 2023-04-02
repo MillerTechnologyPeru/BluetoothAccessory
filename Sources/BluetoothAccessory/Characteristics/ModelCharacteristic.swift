80f8f525c0ab15e60c79f506168cf85496e68388
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-01 21:24:16 -0700

    Add `CentralManager.readModel()`

--- Sources/BluetoothAccessory/Characteristics/ModelCharacteristic.swift
+++ Sources/BluetoothAccessory/Characteristics/ModelCharacteristic.swift
@@ -21,3 +21,25 @@ public struct ModelCharacteristic: Equatable, Hashable, AccessoryCharacteristic 
     
     public var value: String
 }
+
+// MARK: - Central
+
+public extension CentralManager {
+    
+    /// Read model.
+    func readModel(
+        characteristic: Characteristic<Peripheral, AttributeID>
+    ) async throws -> String {
+        let characteristic = try await read(ModelCharacteristic.self, characteristic: characteristic)
+        return characteristic.value
+    }
+}
+
+public extension GATTConnection {
+    
+    /// Read model.
+    func readModel() async throws -> String {
+        let characteristic = try self.cache.characteristic(.model, service: .information)
+        return try await self.central.readModel(characteristic: characteristic)
+    }
+}
