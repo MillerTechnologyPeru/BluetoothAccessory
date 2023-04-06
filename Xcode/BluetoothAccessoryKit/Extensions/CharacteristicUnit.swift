f4bdf3b7d52ca7c591fb34aab8cfda75897059f2
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-04-06 14:56:03 -0700

    Add `CharacteristicUnit.symbol`

---
+++ Xcode/BluetoothAccessoryKit/Extensions/CharacteristicUnit.swift
@@ -0,0 +1,45 @@
+//
+//  CharacteristicUnit.swift
+//  BluetoothAccessoryKit
+//
+//  Created by Alsey Coleman Miller on 4/6/23.
+//
+
+import Foundation
+import BluetoothAccessory
+
+public extension CharacteristicUnit {
+    
+    func description(for value: CharacteristicValue) -> String {
+        value.description + symbol
+    }
+    
+    var symbol: String {
+        switch self {
+        case .arcdegrees:
+            return "°"
+        case .celsius:
+            return "°C"
+        case .fahrenheit:
+            return "°F"
+        case .lux:
+            return "lx"
+        case .microgramsPerMCubed:
+            return "µg/m3"
+        case .percentage:
+            return "%"
+        case .ppm:
+            return "ppm"
+        case .seconds:
+            return "s"
+        case .watts:
+            return "W"
+        case .amps:
+            return "A"
+        case .volts:
+            return "V"
+        case .hertz:
+            return "Hz"
+        }
+    }
+}
