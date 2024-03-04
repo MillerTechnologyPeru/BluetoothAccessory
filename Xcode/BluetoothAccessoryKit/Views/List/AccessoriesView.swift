a02271293ee08e5a4c7374b4030b3c83e3356694
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-03-04 06:22:46 -0500

    Add `AccessoriesView`

---
+++ Xcode/BluetoothAccessoryKit/Views/List/AccessoriesView.swift
@@ -0,0 +1,81 @@
+//
+//  AccessoriesView.swift
+//  BluetoothAccessoryKit
+//
+//  Created by Alsey Coleman Miller on 3/4/24.
+//
+
+import Foundation
+import SwiftUI
+import Bluetooth
+import GATT
+import DarwinGATT
+import BluetoothAccessory
+import SFSafeSymbols
+
+/// List of Paired Accessories
+public struct AccessoriesView: View {
+    
+    @EnvironmentObject
+    private var store: AccessoryManager
+    
+    @State
+    var showSetup = false
+    
+    /*
+    @Binding
+    var showSetup: Bool
+    
+    public init(showSetup: Binding<Bool>) {
+        _showSetup = showSetup
+    }
+    */
+    
+    public init() { }
+    
+    public var body: some View {
+        VStack {
+            if accessories.isEmpty {
+                VStack {
+                    Text("No paired accessories")
+                }
+            } else {
+                List {
+                    ForEach(accessories) { accessory in
+                        AccessoryRow(accessory: accessory)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Devices")
+        .toolbar {
+            Button(action: {
+                add()
+            }, label: {
+                Image(systemSymbol: .plus)
+            })
+        }
+        .sheet(isPresented: $showSetup) {
+            NavigationView {
+                SetupAccessoryView()
+            }
+        }
+    }
+}
+
+private extension AccessoriesView {
+    
+    var accessories: [PairedAccessory] {
+        store.cache.values
+            .sorted(by: { $0.information.type.rawValue < $1.information.type.rawValue })
+            .sorted(by: { $0.name < $1.name })
+    }
+    
+    func add() {
+        showSetup = true
+    }
+}
+
+#Preview {
+    AccessoriesView()
+}
